af6f372d79cbff0d92586f5887d4fb4efe182c6e
carrein <carreinlee@proton.me>
2025-02-25 22:06:54 +0800

    Simplify bootstrapping app.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,58 +1,13 @@
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { createTRPCClient, httpBatchLink } from "@trpc/client";
-import { useState } from "react";
-import { AppRouter } from "../../server/src/appRouter";
+import { QueryClientProvider } from "@tanstack/react-query";
 import { Home } from "./Home";
 import { GlobalStyles } from "./styles/globalStyles";
-import { TRPCProvider } from "./trpc";
+import { queryClient } from "./trpc";
 
-function makeQueryClient() {
-  return new QueryClient({
-    defaultOptions: {
-      queries: {
-        // With SSR, we usually want to set some default staleTime
-        // above 0 to avoid refetching immediately on the client
-        staleTime: 60 * 1000,
-      },
-    },
-  });
-}
-
-let browserQueryClient: QueryClient | undefined = undefined;
-function getQueryClient() {
-  if (typeof window === "undefined") {
-    // Server: always make a new query client
-    return makeQueryClient();
-  } else {
-    // Browser: make a new query client if we don't already have one
-    // This is very important, so we don't re-make a new client if React
-    // suspends during the initial render. This may not be needed if we
-    // have a suspense boundary BELOW the creation of the query client
-    if (!browserQueryClient) browserQueryClient = makeQueryClient();
-    return browserQueryClient;
-  }
-}
-
-export function App() {
-  const queryClient = getQueryClient();
-  const [trpcClient] = useState(() =>
-    createTRPCClient<AppRouter>({
-      links: [
-        httpBatchLink({
-          url: "http://localhost:4000/trpc",
-        }),
-      ],
-    })
-  );
-
-  return (
-    <>
-      <GlobalStyles />
-      <QueryClientProvider client={queryClient}>
-        <TRPCProvider trpcClient={trpcClient} queryClient={queryClient}>
-          <Home />
-        </TRPCProvider>
-      </QueryClientProvider>
-    </>
-  );
-}
+export const App = () => (
+  <>
+    <GlobalStyles />
+    <QueryClientProvider client={queryClient}>
+      <Home />
+    </QueryClientProvider>
+  </>
+);
